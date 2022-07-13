70a759c75f8c0e688c01c9a0dabc7ed4d36ae5cf
swa5 <64044963+swa5@users.noreply.github.com>
2022-07-13 07:02:39 -0700

    Add files via upload

---
+++ grocery.py
@@ -0,0 +1,53 @@
+"""
+Copyright MIT BWSI Autonomous RACECAR Course
+MIT License
+Summer 2022
+
+Code Clash #3 - Combined Grocery List (grocery.py)
+
+
+Author: Ainsley Ward
+
+Difficulty Level: 2/10
+
+Prompt: Sarah’s parents each gave her a grocery list to take with her to Trader Joes. 
+She wants to quickly combine the lists into one, getting rid of duplicate items. 
+Write a function that takes the two grocery lists (two strings) and returns the final 
+grocery list (a list) that does not include duplicate items.
+
+Test Cases:
+
+Input: str1 = 'apples bananas bread sauce onions'; str2 = 'chocolate apples grapes bread';
+Output: [apples, bananas, bread, sauce, onions, chocolate, grapes]
+
+Input: str1 = 'apples bananas bread bananas'; str2 = 'grapes';
+Output: [apples, bananas, bread, grapes]
+
+Input: str1 = 'apples bananas bread bananas'; str2 = '';
+Output: [apples, bananas, bread]
+"""
+
+class Solution:
+    def my_grocery_list(self,str1,str2):
+        # type str1:string
+        # type str2: string
+        # return: list
+
+        # TODO: Write code below to return a list with the solution to the prompt
+        returnlist = []
+        returnlist.append(str1)
+        if str1 != str2:
+            returnlist.append(str2)
+            return returnlist
+        return returnlist
+
+def main():
+    string1 = input()
+    string2 = input()
+
+    tc1 = Solution()
+    ans = tc1.my_grocery_list(string1,string2)
+    print(ans)
+
+if __name__ == "__main__":
+     main()
